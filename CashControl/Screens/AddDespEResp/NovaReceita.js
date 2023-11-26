5e1777bab4e640ba0da9cb9decb346b93c75a2ff
gabrielsb18 <gbmodelator@gmail.com>
2023-11-26 13:32:03 -0300

    input mask added without value and data

--- CashControl/Screens/AddDespEResp/NovaReceita.js
+++ CashControl/Screens/AddDespEResp/NovaReceita.js
@@ -11,6 +11,7 @@ import Receitas from "../Transactions/Receitas";
 import Header from "../../src/components/Header/Header";
 import { enGB, registerTranslation } from 'react-native-paper-dates'
 registerTranslation('en-GB', enGB)
+import { TextInputMask } from 'react-native-masked-text';
 
 import {useForm, Controller} from 'react-hook-form'
 import {yupResolver} from '@hookform/resolvers/yup'
@@ -65,172 +66,198 @@ const NovaReceita = () => {
   }
 
   return (
-    <><Header title="Nova Receita" /><View style={style.container}>
-      <View>
-        <View style={style.input}>
-        <Controller
-          control = {control}
-          name = "nomeReceita"
-          render = {({field :{onChange, value}}) => ( <SelectList
-            style={style.input}
-            setSelected={(val) => onChange(val)}
-            data={dataListReceitas}
-            save="value"
-            value = {value}
-            placeholder="Categoria"
-            boxStyles={{
-              borderRadius: 10,
-              backgroundColor: "white",
-              borderWidth: 1.2,
+    <>
+      <Header title="Nova Receita" />
+      <View style={style.container}>
+        <View>
+          <View style={style.input}>
+            <Controller
+              control={control}
+              name="nomeReceita"
+              render={({ field: { onChange, value } }) => (
+                <SelectList
+                  style={style.input}
+                  setSelected={(val) => onChange(val)}
+                  data={dataListReceitas}
+                  save="value"
+                  value={value}
+                  placeholder="Categoria"
+                  boxStyles={{
+                    borderRadius: 10,
+                    backgroundColor: "white",
+                    borderWidth: 1.2,
 
-              borderWidth: errors.Descricao && 3,
-              borderColor: errors.Descricao && "red",
-            }}
-            inputStyles={{ fontSize: 15 }}
-            dropdownItemStyles={{
-              backgroundColor: "#9BF500",
-              flexDirection: "row",
-              borderRadius: 19,
-              height: 62,
-              marginBottom: 20,
-              marginLeft: 10,
-              marginRight: 10,
-              fontSize: 18,
-              alignItems: "center",
-            }}
-            maxHeight={300} />)}/>
+                    borderWidth: errors.Descricao && 3,
+                    borderColor: errors.Descricao && "red",
+                  }}
+                  inputStyles={{ fontSize: 15 }}
+                  dropdownItemStyles={{
+                    backgroundColor: "#9BF500",
+                    flexDirection: "row",
+                    borderRadius: 19,
+                    height: 62,
+                    marginBottom: 20,
+                    marginLeft: 10,
+                    marginRight: 10,
+                    fontSize: 18,
+                    alignItems: "center",
+                  }}
+                  maxHeight={300}
+                />
+              )}
+            />
 
             {/* Estilização da minha mensagem de erro */}
-            {errors.nomeReceita && <Text style = {style.labelError}>{errors.nomeReceita?.message}</Text>} 
-        </View>
+            {errors.nomeReceita && (
+              <Text style={style.labelError}>
+                {errors.nomeReceita?.message}
+              </Text>
+            )}
+          </View>
 
-        <View>
-          {/* Passei meu text input para dentero do controller */}
-          <Controller
-          control = {control}
-          name = "valorReceita"
-          render = {({field :{onChange, value}}) => (
-            <TextInput
-            style={{height: 45, marginBottom: 10 }}
-            mode="outlined"
-            outlineStyle={{
+          <View>
+            {/* Passei meu text input para dentero do controller */}
+            <Controller
+              control={control}
+              name="valorReceita"
+              render={({ field: { onChange, value } }) => (
+                <TextInput
+                  style={{ height: 45, marginBottom: 10 }}
+                  mode="outlined"
+                  outlineStyle={{
+                    borderRadius: 10,
+                    backgroundColor: "white",
+                    borderWidth: 1.2,
+                    borderWidth: errors.Descricao && 3,
+                    borderColor: errors.Descricao && "red",
+                  }}
+                  onChangeText={onChange}
+                  render={(props) => (
+                    <TextInputMask
+                      {...props}
+                      type={"money"}
+                      options={{
+                        maskType: "BRL"
+                      }}
+                    />
+                  )}
+                  value={value}
+                  keyboardType="phone-pad"
+                  label="Valor"
+                  activeOutlineColor="#75B700"
+                  inputStyles={{ fontSize: 15, color: "#828387" }}
+                />
+              )}
+            />
+            {errors.valorReceita && (
+              <Text style={style.labelError}>
+                {errors.valorReceita?.message}
+              </Text>
+            )}
+          </View>
+
+          <View>
+            <Controller
+              control={control}
+              name="Descricao"
+              render={({ field: { onChange, value } }) => (
+                <TextInput
+                  style={{ marginBottom: 10, height: 45 }}
+                  onChangeText={onChange}
+                  value={value}
+                  outlineStyle={{
+                    borderRadius: 10,
+                    backgroundColor: "white",
+                    borderWidth: 1.2,
+                    color: "#828387",
+
+                    borderWidth: errors.Descricao && 3,
+                    borderColor: errors.Descricao && "red",
+                  }}
+                  mode="outlined"
+                  label="Descrição"
+                  activeOutlineColor="#75B700"
+                  inputStyles={{ fontSize: 15, color: "#828387" }}
+                />
+              )}
+            />
+            {errors.Descricao && (
+              <Text style={style.labelError}>{errors.Descricao?.message}</Text>
+            )}
+          </View>
+          <View>
+            {/* Passei meu text input para dentero do controller */}
+            <Controller
+              control={control}
+              name="Date"
+              render={({ field: { onChange, value } }) => (
+                <TextInput
+                  style={{ height: 45, marginBottom: 10 }}
+                  mode="outlined"
+                  outlineStyle={{
+                    borderRadius: 10,
+                    backgroundColor: "white",
+                    borderWidth: 1.2,
+
+                    borderWidth: errors.Descricao && 3,
+                    borderColor: errors.Descricao && "red",
+                  }}
+                  render={(props) => (
+                    <TextInputMask
+                      {...props}
+                      type={"datetime"}
+                      options={{
+                        format: "DD/MM/YYYY",
+                      }}
+                    />
+                  )}
+                  value={value}
+                  onChangeText={(text) => onChange(text)}
+                  label="Data"
+                  activeOutlineColor="#75B700"
+                  inputStyles={{ fontSize: 15, color: "#828387" }}
+                />
+              )}
+            />
+            {errors.Date && (
+              <Text style={style.labelError}>{errors.Date?.message}</Text>
+            )}
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              height: 50,
+              width: 250,
+              borderWidth: 0,
               borderRadius: 10,
+
+              marginTop: 25,
+              padding: 10,
+
               backgroundColor: "white",
-              borderWidth: 1.2,
-              borderWidth: errors.Descricao && 3,
-              borderColor: errors.Descricao && "red",
             }}
-            onChangeText={onChange}
-            value = {value}
-            keyboardType="phone-pad"
-            // value={Text}
-            // placeholder = 'Valor'
-            label="Valor"
-            // value={valor}
-            // onChangeText={(text) => setValor(text)}
-            activeOutlineColor="#75B700"
-            inputStyles={{ fontSize: 15, color: "#828387" }} />
-          )}/>
-          {errors.valorReceita && <Text style = {style.labelError}>{errors.valorReceita?.message}</Text>} 
-        </View>
+          >
+            <Checkbox
+              status={checked ? "checked" : "unchecked"}
+              onPress={() => {
+                setChecked(!checked);
+              }}
+              color="green"
+            />
+            <Text>Lembrete</Text>
+          </View>
 
-        <View>
-        <Controller
-          control = {control}
-          name = "Descricao"
-          render = {({field :{onChange, value}}) => (          
-          <TextInput
-            style={{ marginBottom: 10, height: 45,}}
-            onChangeText={onChange}
-            value = {value}
-            //descriptionHeight 
-            outlineStyle={{
-              borderRadius: 10,
-              backgroundColor: "white",
-              borderWidth: 1.2,
-              color: "#828387",
-
-              borderWidth: errors.Descricao && 3,
-              borderColor: errors.Descricao && "red",
-            }}
-            mode="outlined"
-            // value={Text}
-            // multiline
-            // onContentSizeChange={(e) => {
-            //   setDescriptionHeight(e.nativeEvent.contentSize.height);
-            // }}
-            // placeholder = 'Decrição'
-            // value={descricao}
-            // onChangeText={(text) => setDescricao(text)}
-            label="Descrição"
-            activeOutlineColor="#75B700"
-            inputStyles={{ fontSize: 15, color: "#828387" }} />)}/>
-            {errors.Descricao && <Text style = {style.labelError}>{errors.Descricao?.message}</Text>} 
-        </View>
-          
-
-        <Controller
-          control = {control}
-          name = "Date"
-          render = {({field :{onChange, value}}) => (
-            <SafeAreaProvider>
-            <View style={style.inputDate}>
-              <DatePickerInput
-                outlineStyle={{
-                  borderRadius: 10,
-                  backgroundColor: "white",
-                  borderWidth: 1.2,
-
-                  borderWidth: errors.Descricao && 3,
-                  borderColor: errors.Descricao && "red",
-                }}
-                locale="en-GB"
-                label="Data"
-                onChange={(d) => onChange(d)}
-                value={value}
-                inputMode="start"
-                style={{ height: 45, marginBottom: 10}}
-                mode="outlined"
-                activeOutlineColor="#75B700"
-                presentationStyle="overFullScreen"
-                transparent={false} />
-                {errors.Date && <Text style = {style.labelError}>{errors.Date?.message}</Text>}
-            </View>
-            </SafeAreaProvider>
-          )}/>
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            height: 50,
-            width: 250,
-            borderWidth: 0,
-            borderRadius: 10,
-
-            marginTop: 75,
-            padding: 10,
-
-            backgroundColor: "white",
-          }}
-        >
-          <Checkbox
-            status={checked ? "checked" : "unchecked"}
-            onPress={() => {
-              setChecked(!checked);
-            } }
-            color="green" />
-          <Text>Lembrete</Text>
-        </View>
-
-        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-          <TouchableOpacity onPress={handleSubmit(handleSignIn)}>
-            <View style={style.Button}>
-              <Text>Adicionar</Text>
-            </View>
-          </TouchableOpacity>
+          <View style={{ justifyContent: "center", alignItems: "center" }}>
+            <TouchableOpacity onPress={handleSubmit(handleSignIn)}>
+              <View style={style.Button}>
+                <Text>Adicionar</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
-    </View></>
+    </>
   );
 };
 
